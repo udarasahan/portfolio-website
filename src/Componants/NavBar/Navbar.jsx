2775c5c05761b339e2620f4fc9ddd88f90cf0465
udarasahan <udarasahanj@gmail.com>
2024-05-31 01:25:51 +0530

    made it responsive

--- src/Componants/NavBar/Navbar.jsx
+++ src/Componants/NavBar/Navbar.jsx
@@ -1,17 +1,30 @@
 import './Navbar.css';
 import logo from '../../assets/logo.png';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import underline from '../../assets/theme_pattern.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import menuIcon from '../../assets/menu_icon.png';
+import menuClose from '../../assets/cross-icon.png';
 
 const Navbar = () => {
 
-  const [menu,setMenu] = useState("home")
+  const [menu,setMenu] = useState("home");
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right="0";
+  }
+  const closeMenu = () => {
+    menuRef.current.style.right="-350px";
+  }
+
 
   return (
     <div className="navbar" id='navbar'>
         <img src={logo} alt="" />
-        <ul className='nav-menu'>
+        <img src={menuIcon} onClick={openMenu} alt="" className='nav-mob-open' />
+        <ul ref={menuRef} className='nav-menu'>
+          <img src={menuClose} onClick={closeMenu} alt="" className="nav-mob-close" />
             <li><AnchorLink className='anchor-link' offset={50} href='#navbar'><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu==="home"?<img src={underline} />:<></>}</li>
             <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")}>About Me</p></AnchorLink>{menu==="about"?<img src={underline} />:<></>}</li>
             <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{menu==="services"?<img src={underline} />:<></>}</li>
