40765694d8f717e43caf0fb3c4c8473a57d7752e
udarasahan <udarasahanj@gmail.com>
2024-06-06 14:19:31 +0530

    header image changed and resume added

--- src/Componants/Header/Header.jsx
+++ src/Componants/Header/Header.jsx
@@ -1,5 +1,5 @@
 import './Header.css';
-import headerImage from '../../assets/header-image-1.jpeg';
+import headerImage from '../../assets/header-image-2.jpg';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Heder = () => {
@@ -7,14 +7,15 @@ const Heder = () => {
     <div className='header' id='home'>
       <img src={headerImage} alt="" />
       <h1><span>I&apos;m Udara Sahan,</span> Full Stack Developer.</h1>
-      <p>I enjoy building dynamic and user friendly web applications and 
-        solving challenging problems. I pay close attention to 
-        detail and prioritize creating great user experiences. 
-        I am dedicated to delivering high-quality solutions and excited 
-        to contribute to innovative projects and work with like-minded professionals.</p>
+      <p>Creating dynamic and user-friendly web applications and solving challenging problems 
+        are at the core of my professional focus. I pay close attention to detail and prioritize 
+        creating great user experiences. I am dedicated to delivering high-quality solutions 
+        and excited to contribute to innovative projects and work with like-minded professionals.</p>
       <div className="header-action">
         <div className="header-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-        <div className='header-resume'>My Resume</div>
+        <div className='header-resume'>
+            <a href="https://drive.google.com/file/d/1TxFcTdRLi1NmJmGEJqH2zunkxJo1JVw3/view" target="_blank">My Resume</a>
+        </div>
       </div>
     </div>
   )
